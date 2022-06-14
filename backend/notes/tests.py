7950398b5c9bd2939c6238cf149758e24daa4ead
fadyayad7 <fady.ayad7@gmail.com>
2022-06-14 13:54:26 +0200

    added test

--- backend/notes/tests.py
+++ backend/notes/tests.py
@@ -1,3 +1,16 @@
+from datetime import datetime
 from django.test import TestCase
+from .models import Note
+
 
 # Create your tests here.
+class TestNotes(TestCase):
+    def test_can_get_notes(self):
+        note = Note(content="note1")
+        note.save()
+        note = Note(content="note2")
+        note.save()
+        self.assertEquals(Note.objects.all().count(), 2)
+        self.assertEquals(Note.objects.all().first().content, 'note1')
+
+
