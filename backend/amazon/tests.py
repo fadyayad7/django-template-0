52881f1cbd44ed1c1c347be1fb6e400ffd7ca7a5
fadyayad7 <fady.ayad7@gmail.com>
2022-06-14 16:06:01 +0200

    added test

--- backend/amazon/tests.py
+++ backend/amazon/tests.py
@@ -1,3 +1,16 @@
+from unicodedata import name
 from django.test import TestCase
+from .models import Product
+
 
 # Create your tests here.
+class TestAmazon(TestCase):
+    def test_can_get_notes(self):
+        product = Product(name="iphone15", description="desc", price=100.00)
+        product.save()
+        product = Product(name="macbook m4 chip", description="desc", price=100.00)
+        product.save()
+        self.assertEquals(Product.objects.all().count(), 2)
+        self.assertEquals(Product.objects.all().first().name, 'iphone15')
+
+
